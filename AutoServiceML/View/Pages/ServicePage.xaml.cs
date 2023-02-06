dd58b4e20bf6f35a5d73de0cc9a56f76e3d42fd1
rotoyouoi <rotoyouoi@yandex.ru>
2023-02-06 13:23:46 +0500

    Добавьте файлы проекта.

---
+++ AutoServiceML/View/Pages/ServicePage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using AutoServiceML.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AutoServiceML.View.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ServicePage.xaml
+    /// </summary>
+    public partial class ServicePage : Page
+    {
+        Core db = new Core();
+        List<Service> arrayService; 
+        public ServicePage()
+        {
+            InitializeComponent();
+            arrayService = db.context.Service.ToList();
+            LViewServices.ItemsSource = arrayService;
+        }
+
+        private void BtnAddServicesClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnEditClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnDeleteClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
